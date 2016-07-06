1c44f9e69a021fb543d0cd78d5dd793a58ffc375
wdn4em <wdn4em@virginia.edu>
2016-07-06 15:31:27 -0400

    Added python2/3 compatiblity

--- hackapp.py
+++ hackapp.py
@@ -1,19 +1,24 @@
 #!flask/bin/python
+try:   #For python 2
+    from urllib2 import Request, urlopen, URLError
+except ImportError:		#For python 3
+	from urllib import request
+	from urllib.request import urlopen
+	from urllib.error import URLError
 
-from urllib2 import Request, urlopen, URLError
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def enter():
-	
+
 	request = Request('http://api.reimaginebanking.com/customers?key=fdcdbc9f4d339cd5970c2d87c8e8d16f')
 
 	try:
 		response = urlopen(request)
 		customers = response.read()
 		return customers
-	except URLError, e:
+	except URLError as e:
 		return "404 errror"
 
 
