d6ed32148e3c824c09a8cbb7c5fdb63d5efcb452
Kwame Asante <kasante.asante@gmail.com>
2016-07-06 16:55:18 -0400

    parsing customer and account info

--- hackapp.py
+++ hackapp.py
@@ -2,6 +2,8 @@
 from urllib2 import Request, urlopen, URLError
 from flask import Flask, render_template, jsonify
 import urllib, json
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 try:   #For python 2
     from urllib2 import Request, urlopen, URLError
@@ -21,8 +23,11 @@ def enter():
 	try:
 		response = urllib.urlopen('http://api.reimaginebanking.com/customers?key=fdcdbc9f4d339cd5970c2d87c8e8d16f')
 		customers = json.loads(response.read())
+		acct_response = urllib.urlopen('http://api.reimaginebanking.com/customers/'+customers[0]['_id']+'/accounts?key=fdcdbc9f4d339cd5970c2d87c8e8d16f')
+		accounts = json.loads(acct_response.read())
 		customer_name = customers[0]['first_name']
-		return render_template('index.html', customer=customer_name)
+		checking = accounts[2]['balance']
+		return render_template('index.html', customer=customer_name, balance=checking)
 	except URLError, e:
 		return "404 error"
 
