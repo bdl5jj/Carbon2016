145aa0857c3b0045ebdca6236cd6ea20e2ce68ca
Kwame Asante <kasante.asante@gmail.com>
2016-07-06 16:04:01 -0400

    templating parsed json

--- hackapp.py
+++ hackapp.py
@@ -1,20 +1,22 @@
 #!flask/bin/python
-
 from urllib2 import Request, urlopen, URLError
-from flask import Flask
+from flask import Flask, render_template, jsonify
+import urllib, json
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def enter():
 	
 	request = Request('http://api.reimaginebanking.com/customers?key=fdcdbc9f4d339cd5970c2d87c8e8d16f')
 
 	try:
-		response = urlopen(request)
-		customers = response.read()
-		return customers
+		response = urllib.urlopen('http://api.reimaginebanking.com/customers?key=fdcdbc9f4d339cd5970c2d87c8e8d16f')
+		customers = json.loads(response.read())
+		customer_name = customers[0]['first_name']
+		return render_template('index.html', customer=customer_name)
 	except URLError, e:
-		return "404 errror"
+		return "404 error"
 
 
 
