ea03d0029e8c2f2c916da957781fb6dacd0f4792
wdn4em <wdn4em@virginia.edu>
2016-07-06 20:40:55 -0400

    took out python3 things

--- hackapp.py
+++ hackapp.py
@@ -5,12 +5,6 @@ import urllib, json
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-try:   #For python 2
-    from urllib2 import Request, urlopen, URLError
-except ImportError:		#For python 3
-	from urllib import request
-	from urllib.request import urlopen
-	from urllib.error import URLError
 
 from flask import Flask
 app = Flask(__name__)
@@ -25,11 +19,13 @@ def enter():
 		customers = json.loads(response.read())
 		acct_response = urllib.urlopen('http://api.reimaginebanking.com/customers/'+customers[0]['_id']+'/accounts?key=fdcdbc9f4d339cd5970c2d87c8e8d16f')
 		accounts = json.loads(acct_response.read())
+        #no idea whats wrong with this
+        pur_response = urllib.urlopen('http://api.reimaginebanking.com/accounts/577d3b200733d0184021f54e/purchases?key=fdcdbc9f4d339cd5970c2d87c8e8d16f')
+        pur = json.loads(pur_response.read())
 		customer_name = customers[0]['first_name']
-		return render_template('index.html', customer=customer_name, accounts=accounts)
+		return render_template('index.html', customer=customer_name, accounts=accounts, purchases=pur)
 	except URLError, e:
 		return "404 error"
-
 
 
 if __name__ == '__main__':
